003d0b27c022fc9e973aabff9ee4766a7aec15fd
Ryan Joseph <160825414+rj7002@users.noreply.github.com>
2024-11-13 13:02:41 -0800

    Update 3dshot.py

--- 3dshot.py
+++ 3dshot.py
@@ -339,8 +339,9 @@ if selected_seasons:
         df = df[(df['MINUTES_REMAINING'] >= timemin) & (df['MINUTES_REMAINING'] <= timemax)]
     if Quarter:
         df = df[df['PERIOD'].isin(quart)]
-    Make = st.sidebar.checkbox('Make Shot Paths',value=True)
-    Miss = st.sidebar.checkbox('Miss Shot Paths')
+    if vids != 1:
+        Make = st.sidebar.checkbox('Make Shot Paths',value=True)
+        Miss = st.sidebar.checkbox('Miss Shot Paths')
 
 
     if selected_seasons:
