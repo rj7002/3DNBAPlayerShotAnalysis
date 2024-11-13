b87544317ba267c87384f21bf738ed7ba61504c8
Ryan Joseph <160825414+rj7002@users.noreply.github.com>
2024-11-13 11:00:32 -0800

    Update 3dshot.py

--- 3dshot.py
+++ 3dshot.py
@@ -1341,7 +1341,7 @@ if selected_seasons:
             ))
             # Update layout for better visuals
             if vids:
-                h = 450
+                h = 460
             else:
                 h = 600
             fig.update_layout(
